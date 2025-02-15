46c8303741c59c4733272454dfb82eeb5391162a
rgo <rzgggg12@gmail.com>
2025-02-15 14:15:59 +0330

    separated translation provider and helper function

--- apps/web/providers/translation-provider.tsx
+++ apps/web/providers/translation-provider.tsx
@@ -1,24 +1,29 @@
-
-import { I18nextProvider } from "react-i18next";
-import initTranslations from "@/app/i18n";
-import {createInstance, Resource} from "i18next";
-import { ReactNode } from "react";
+import {getServerSideTranslations} from "@/hooks/get-ssr-translations";
 import Resources from "@/@types/resources";
+import {ReactNode} from "react";
+import {I18NTranslationProvider} from "@/providers/I18N-translation-provider";
 
-export default function TranslationsProvider({
-                                                 children,
-                                                 locale,
-                                                 namespaces,
-                                                 resources,
-                                             }: Readonly<{
-    children: ReactNode;
-    locale: string;
-    namespaces: Array<keyof Resources>;
-    resources: Resource;
-}>) {
-    const i18n = createInstance();
-
-    initTranslations(locale, namespaces, i18n, resources);
-
-    return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
+interface Params {
+  locale: string;
+  ns?: Array<keyof Resources>;
+  children: ReactNode;
 }
+export const globalNs: Array<keyof Resources> = ["common"];
+
+export default async function TranslationProvider({ locale, ns, children }: Params) {
+  const { resources } = await getServerSideTranslations({
+    locale: locale,
+    ns: globalNs.concat(ns ?? []),
+  });
+
+  return (
+    <I18NTranslationProvider
+      namespaces={globalNs.concat(ns ?? [])}
+      locale={locale}
+      resources={resources}
+    >
+      {children}
+    </I18NTranslationProvider>
+  );
+}
+

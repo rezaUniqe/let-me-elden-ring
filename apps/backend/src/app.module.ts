2abcbc888f98e1c49abac21f20884a89b9d8718f
rgo <rzgggg12@gmail.com>
2025-02-19 11:25:07 +0330

    dev script added to backend app

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -9,7 +9,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'database',
+      host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT!),
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,

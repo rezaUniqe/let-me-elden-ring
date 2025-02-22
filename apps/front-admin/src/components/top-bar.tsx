9bb4c997a00302d75c2d4c511ecc7fed3a2af7da
rgo <rzgggg12@gmail.com>
2025-02-22 13:38:23 +0330

    top bar added

---
+++ apps/front-admin/src/components/top-bar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from "@workspace/ui/components/button";
+
+const TopBar = () => {
+  return (
+    <div
+      className={
+        "absolute shadow-md px-5 bg-black/24 h-[50px] w-full flex flex-row items-center"
+      }
+    >
+      <Button className={"ml-auto"} variant={"secondary"}>
+        Login
+      </Button>
+    </div>
+  );
+};
+
+export default TopBar;

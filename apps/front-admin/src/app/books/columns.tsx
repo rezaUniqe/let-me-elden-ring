047ff321e06cbda75afdf165a2e0dc3eea952c3b
rgo <rzgggg12@gmail.com>
2025-02-23 09:01:14 +0330

    table columns added

--- apps/front-admin/src/app/books/columns.tsx
+++ apps/front-admin/src/app/books/columns.tsx
@@ -1,49 +1,56 @@
 "use client";
 
-import {ColumnDef} from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
-import {Book} from "@/model/book";
-import {Button} from "@workspace/ui/components/button";
-import {Delete, DeleteIcon, Edit, TrashIcon} from "lucide-react";
-
+import { Book } from "@/model/book";
+import { Button } from "@workspace/ui/components/button";
+import { Edit, TrashIcon } from "lucide-react";
 
 export const columns: ColumnDef<Book>[] = [
-    {
-        accessorKey: "id",
-        header: "ID",
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    header: "Title",
+    id: "image-title",
+    cell: ({ row }) => {
+      return (
+        <div className={"flex flex-row gap-3 items-center"}>
+          <Image
+            width={35}
+            height={35}
+            className={"rounded"}
+            src={row.original.coverImage}
+            alt={"book image"}
+          />
+          <p>{row.original.title}</p>
+        </div>
+      );
     },
-    {
-        header: "Title",
-        id:"image-title",
-        cell: ({row}) => {
-            return <div className={"flex flex-row gap-3 items-center"}>
-                <Image width={35} height={35} className={"rounded"} src={row.original.coverImage} alt={"book image"}/>
-                <p>
-                    {row.original.title}
-                </p>
-            </div>
-        },
+  },
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    accessorKey: "author",
+    header: "Author",
+  },
+  {
+    id: "action",
+    header: "",
+    cell: () => {
+      return (
+        <div className={"flex flex-row gap-1 w-fit"}>
+          <Button variant={"ghost"}>
+            <Edit />
+          </Button>
+          <Button variant={"ghost"}>
+            <TrashIcon color={"red"} />
+          </Button>
+        </div>
+      );
     },
-    {
-        accessorKey: "description",
-        header: "Description",
-    },
-    {
-        accessorKey: "author",
-        header: "Author",
-    },
-    {
-        id:"action",
-        header: "",
-        cell: () => {
-            return <div className={"flex flex-row w-fit"}>
-                <Button>
-                    <Edit/>
-                </Button>
-                <Button>
-                    <TrashIcon color={"red"}/>
-                </Button>
-            </div>
-        }
-    },
+  },
 ];

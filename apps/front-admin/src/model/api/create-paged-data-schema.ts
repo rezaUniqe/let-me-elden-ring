f050c5dce8a4334b597c61ba0cd3372fac0f34fb
rgo <rzgggg12@gmail.com>
2025-02-25 09:07:53 +0330

    added PagedApiData type

--- apps/front-admin/src/model/api/create-paged-data-schema.ts
+++ apps/front-admin/src/model/api/create-paged-data-schema.ts
@@ -1,9 +1,14 @@
 import { z, ZodType } from "zod";
 
-export const createPagedDataSchema = <T>(schema: ZodType<T>) =>
-  z.object({
+export function createPagedDataSchema<T>(schema: ZodType<T>) {
+  return z.object({
     items: schema.array(),
     page: z.number().default(1),
     perPage: z.number().default(6),
     totalPages: z.number(),
+    totalItems: z.number(),
   });
+}
+export type PagedApiData<T> = z.infer<
+  ReturnType<typeof createPagedDataSchema<T>>
+>;

cd632f10111c271b38f7b13eb9f31764efe21dfd
rgo <rzgggg12@gmail.com>
2025-04-10 14:57:54 +0330

    added handling login form

--- apps/front/src/app/[locale]/(auth)/login/hooks/use-handle-login-form.ts
+++ apps/front/src/app/[locale]/(auth)/login/hooks/use-handle-login-form.ts
@@ -14,7 +14,7 @@ export function useHandleLoginForm() {
     },
   });
 
-  const onLoginFormSubmit = form.handleSubmit((data) => {
+  const onLoginFormSubmit = form.handleSubmit(() => {
     try {
       // toast({
       //   title: "Success",

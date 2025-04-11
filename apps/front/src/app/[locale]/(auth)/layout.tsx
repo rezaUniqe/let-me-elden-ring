fc974ca1e1ff3b7653cdcce1ff1dc6603ead6968
Ersan <ersan@zed-pay.com>
2025-04-11 17:22:41 +0330

    moved all schema to its own folder

--- apps/front/src/app/[locale]/(auth)/layout.tsx
+++ apps/front/src/app/[locale]/(auth)/layout.tsx
@@ -1,16 +1,8 @@
 import { ReactNode } from "react";
-import Link from "next/link";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-muted/40 py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="flex justify-center">
-          <Link href="/" className="flex items-center space-x-2">
-            <span className="text-2xl font-bold">YourApp</span>
-          </Link>
-        </div>
-      </div>
+    <div className="w-screen flex min-h-screen flex-col items-center justify-center bg-muted/40 py-12 sm:px-6 lg:px-8">
       {children}
     </div>
   );
